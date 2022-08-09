56ec49da4651efa9aa383ba6c534c9e3c2a6fb1d
Oleg Mykhaylovskyy <ttest261@gmail.com>
2022-08-09 23:49:04 +0300

    Add HW1 - two files

---
+++ HW1/OlegM/1.py
@@ -0,0 +1,9 @@
+# Homework 1.1.
+
+name = input('What is your name?:')
+age = int(input('How old are you?:'))
+city = input('Where do you live?:')
+
+print(f'Hello, {name}. ', end="")
+print(f'Your age is {age}. ', end="")
+print(f'You live in {city}.')
---
+++ HW1/OlegM/2.py
@@ -0,0 +1,19 @@
+a = float(input("Enter first number:"))
+b = float(input("Enter second number:"))
+#сума чисел
+amount = a + b
+print(f'Amount = {amount}')
+#різниця чисел
+minus = a - b
+print(f'Minus = {minus}')
+#добуток чисел чисел
+multi = a * b
+print(f'Multi = {multi}')
+#ділення чисел
+division = a / b
+print(f'Division = {round(division, 2)}')
+#
+degree = a ** b
+print(f'Degree = {round(degree, 2)}')
+
+
