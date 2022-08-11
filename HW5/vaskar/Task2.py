f67515087661c15f8cb264a4280d18b5474fd614
Basil <karbanvasyl@gmail.com>
2022-08-11 14:41:53 +0300

    tasks

--- HW5/vaskar/Task2.py
+++ HW5/vaskar/Task2.py
@@ -1,25 +1,5 @@
-iterations = int(input("How many iterations do you want?"))
-start = 0
-initial = 1
-iters = 1
-#iterations for advanced users
-if iterations == 0:
-    print("You know what, you do know what Fibonacci sequence is. Stop playing with our expensive software \
-        and enter number of iterations >0 in natural numbers")
-else:
-    while iters <=iterations:
-        fibo = start + initial
-        start = initial
-        initial = fibo
-        print(fibo)
-        iters +=1
-# up to some number
-end = int(input("Up to what number would you like to calcilate Fibonacci sequense?"))
-start = 0
-initial = 1
-fibo = 0
-while fibo <=end:
-    fibo = start + initial
-    start = initial
-    print(initial)
-    initial = fibo
+numbers = [1,2,3,4,5,6,7,8,9,10,11]
+numbers = [float(a) for a in numbers]
+print(numbers)
+lister = [int(x) for x in input("enter them!").split(",")]
+print(lister)
