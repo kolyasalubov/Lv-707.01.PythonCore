279432312e29ce1bb2f87b5dd188d6959aed06a8
denys <dp070999eda1@gmail.com>
2022-08-11 14:26:27 +0300

    add HW 1-6

---
+++ HW6/mabel/2.py
@@ -0,0 +1,17 @@
+a = []
+b = []
+c = []
+
+
+for i in range(1, 10):
+    if i %3 != 0 and i %2 != 0 :
+        a.append(i)
+    elif i %3 == 0: 
+        b.append(i)
+    elif i %2 == 0:
+        c.append(i)
+    
+print(f'Числа которые не деляться на 3 и на 2 {a}')
+print(f'Числа которые деляться на 3 {b}')
+print(f'Числа которые деляться на 3 {c}')
+
