fadf902c0f770faa5966b4f6b84aa2a821707463
Mariia Onyshchenko <arikado969@gmail.com>
2022-08-13 23:46:42 +0300

    added homework 7

---
+++ HW7/marony/Convert a Number to a String.py
@@ -0,0 +1,4 @@
+def number_to_string(num):
+    num = str(num)
+    print (f'Number in string format: {num}, and verification: it is {type(num)}')
+number_to_string(int(input('Enter your number: ')))
