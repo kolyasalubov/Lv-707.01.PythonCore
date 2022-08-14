09ccc6d645869d6243415e0f80f7d7928c8c6766
Taras Prynda <iprynda@ukr.net>
2022-08-14 20:28:49 +0200

    Added directiries with HW1-7 with the homeworks from branch TarasPrynda

---
+++ HW6/TPrynda/Prynda_Taras_HW_6.1.py
@@ -0,0 +1,10 @@
+list=range(1,10)
+
+
+for i in list:
+    if i%2==0:
+        print(f"Number {i} - is EVEN number divisible by 2")
+    elif i%3==0:
+        print(f"Number {i} - is ODD number divisible by 3")
+    elif i%3>0 and i%2>0:
+        print(f"Number {i} - is not divisible by 2 and 3")
