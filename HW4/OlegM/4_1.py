d8aeeb21b1ab4895ce13b2f6198a5b0ffc2e2874
Oleg Mykhaylovskyy <ttest261@gmail.com>
2022-08-16 21:03:29 +0300

    add logic for negative numbers, 1 and 0  in hw4

--- HW4/OlegM/4_1.py
+++ HW4/OlegM/4_1.py
@@ -5,6 +5,11 @@ without using recursion.'''
 
 values: int = int(input("Enter number for calculate the factorial:"))
 result: float = 1
-for i in range(1, values+1):
-    result *= i
-print(f"Factorial of {values} = {result}")
+if values < 0:
+    print(f"Factorial of: {values} not exist, {values} is negative number")
+elif values == 0 or values == 1:
+    print(f"Factorial of {values} = {result}")
+elif values > 1:
+    for i in range(1, values+1):
+        result *= i
+    print(f"Factorial of {values} = {result}")
