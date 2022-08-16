d8e96390e17213703ddd3e8847c3887d809145f5
Oleg Mykhaylovskyy <ttest261@gmail.com>
2022-08-16 21:27:13 +0300

    change logic for create 3 alone function in hw7_file_7_2

--- HW7/OlegM/7_2.py
+++ HW7/OlegM/7_2.py
@@ -1,32 +1,41 @@
-def find_area(a: int, b: int = 0, c: int = 0, type_of_figure: str = "circle") -> float:
-    '''
-    calculates the area of a rectangle, triangle and circle
-    :param a: radius or side a of rectangle or side a of triangle
-    :param b: side b of rectangle or side b of triangle
-    :param c: side c of triangle
-    :param type_of_figure: rectangle, triangle and circle for calculates the area
-    :return:  the area of a rectangle, triangle and circle
-    '''
-    match type_of_figure:
-        case 'circle':
-            return 3.141592 * a ** 2
-        case 'rectangle':
-            return a * b
-        case 'triangle':
-            p = (a + b + c) / 2
-            return (p * (p - a) * (p - b) * (p - c)) ** 0.5
+import math
 
 
-type_of_operation = input("Enter type of figure for calculate area(rectangle, triangle or circle):")
+def find_area_circle(radius: int) -> float:
+    '''
+    :param radius: radius of circle
+    :return: area of circle
+    '''
+    return math.pi * radius ** 2
+
+
+def find_area_rect(weight: int, height: int) -> float:
+    '''
+    :param weight, height: weight and height of rect
+    :return: area of rect
+    '''
+    return weight * height
+
+
+def find_area_triangle(a: int, b: int, c: int) -> float:
+    '''
+    :param a, b, c: side of triangle
+    :return: area of triangle
+    '''
+    p = (a + b + c) / 2
+    return (p * (p - a) * (p - b) * (p - c)) ** 0.5
+
+
+type_of_operation = input("Enter type of figure for calculate area(1-rectangle, 2-triangle or 3-circle):")
 match type_of_operation:
-    case 'circle':
+    case 'circle' | '3':
         r = int(input('Enter radius of circle:'))
-        print(f"Area of circle = {find_area(r, type_of_figure='circle')}")
-    case 'rectangle':
+        print(f"Area of circle = {round(find_area_circle(r), 2)}")
+    case 'rectangle' | '1':
         a = (int(num) for num in input("Enter side a and b of rectangle use space between:").split())
-        print(f"Area of rectangle = {find_area(*a, type_of_figure='rectangle')}")
-    case 'triangle':
+        print(f"Area of rectangle = {round(find_area_rect(*a), 2)}")
+    case 'triangle' | '2':
         a = (int(num) for num in input("Enter side a, b, c of triangle use space between:").split())
-        print(f"Area of triangle = {find_area(*a, type_of_figure='triangle')}")
+        print(f"Area of triangle = {round(find_area_triangle(*a), 2)}")
     case _:
-        print('Program error!')
+        print('Not correct choice! Program error!')
