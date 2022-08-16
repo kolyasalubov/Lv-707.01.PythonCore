b26a86b6fb7bf96dd526e844b601f674968500a4
Vasyl Shulevka <shulvasya@gmail.com>
2022-08-16 12:21:53 +0300

    fixed 3.py

--- HW4/shvasya/3.py
+++ HW4/shvasya/3.py
@@ -1,13 +1,16 @@
 n = int(input("Enter a number for fibonacci formula:"))
 
-print(f"fib number is {n}" if n == 0 else f"fib number is {n}")
-
 i = 1
 fib1 = 0
 fib2 = 1
 
-for i in range(0, n):
-    fib = fib1 + fib2
-    fib1 = fib2
-    fib2 = fib
-    print(fib, end = ' ')
+for i in range(1, n):
+    if i > 0:
+        fib = fib1 + fib2
+        fib1 = fib2
+        fib2 = fib
+        print("fib numbers are",fib, end = ' ')
+else:
+    if n == 0:
+        print("fib number is 0")
+# I couldn't make it work with the following construction print(f"fib number is {n}" if n == 0 else f" fib number is {fib}")    
