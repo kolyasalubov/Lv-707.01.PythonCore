90bb77dba7f5b981735e9e8e44577a6222c96f12
Volodymyr Dzhupyna <vdzhupyna@gmail.com>
2022-08-17 15:08:08 +0300

    added a check if user will enter 1 or 0

--- HW4/vdzhup/1.py
+++ HW4/vdzhup/1.py
@@ -1,10 +1,12 @@
-###### first way (using "while") #######
+##### first way (using "while") #######
 
 user_factorial = int(input("Enter only the positive number for the factorial: "))
 
 factorial_count = 1
 
-if user_factorial < 0:
+if user_factorial == 1 or user_factorial == 0:
+    print(f"The factorial of the number {user_factorial}! is: {factorial_count}")
+elif user_factorial < 0:
     print(f"{user_factorial} is non positive number!")
 else:
     while user_factorial > 1:
@@ -20,7 +22,9 @@ user__factorial = int(input("Enter only the positive number for the factorial: "
 
 factorial__count = 1
 
-if user__factorial < 0:
+if user__factorial == 1 or user__factorial == 0:
+    print(f"The factorial of the number {user__factorial}! is: {factorial__count}")
+elif user__factorial < 0:
     print(f"{user__factorial} is non positive number!")   
 else:
     for i in range(1, user__factorial + 1):
