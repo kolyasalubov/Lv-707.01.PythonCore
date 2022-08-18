cd123c714f5c2f85d380e8198cfc7ec5d8916e84
Vasyl Shulevka <shulvasya@gmail.com>
2022-08-18 11:21:40 +0300

    added 2 lines

--- HW5/shvasya/1.py
+++ HW5/shvasya/1.py
@@ -1,10 +1,11 @@
-for i in range(1,11):
-    if i % 3 == 0 and i != 6:
-        print(i, end = ' ')
 
 for m in range(1,11):
     if m % 2 == 0:
         print(m, end = ' ')
+
+for i in range(1,11):
+    if i % 3 == 0 and i != 6:
+        print(i, end = ' ')
 
 for n in range(1,11):
     if n % 3 != 0 and n % 2 != 0:
