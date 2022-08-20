6201bb7a84c5da2b1c466d96633d1b7e3aa4d99f
Yan Muravskyi <phoenix3999@gmail.com>
2022-08-20 17:43:04 +0300

    HW1 Task#2

---
+++ HW1/YanMur/task_2.py
@@ -0,0 +1,10 @@
+# Task 2
+
+x = float(input("Input x: \n"))
+y = float(input("Input y: \n"))
+
+print(f"x + y = {x + y}")
+print(f"x - y = {x - y}")
+print(f"x * y = {x * y}")
+print(f"x / y = {x / y}")
+print(f"x ^ y = {x ** y}")
