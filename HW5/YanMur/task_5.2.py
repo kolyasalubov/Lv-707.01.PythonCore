9285b8bf9080cdf2c8d98029472b4f474a01b325
Yan Muravskyi <phoenix3999@gmail.com>
2022-08-21 16:15:35 +0300

    HW5 Task#2

---
+++ HW5/YanMur/task_5.2.py
@@ -0,0 +1,10 @@
+# Task 3
+number = int(input("Input fibonacci value: "))
+n = 1
+fibonacci = [0, 1]
+
+while fibonacci[n] + fibonacci[n - 1] <= number:
+    fibonacci.insert(n + 1, fibonacci[n] + fibonacci[n - 1])
+    n += 1
+
+print(f"Fibonacci list {fibonacci}")
