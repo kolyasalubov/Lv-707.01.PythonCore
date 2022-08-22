9f67f05af91a9c8e0a513bdd611df55db201c0da
Vasyl Shulevka <shulvasya@gmail.com>
2022-08-22 15:52:48 +0300

    added homework 8

---
+++ HW8/shvasya/calculator.py
@@ -0,0 +1,19 @@
+def func_summary():
+    a = float(input("Enter a first number:"))
+    b = float(input("Enter a second number:"))
+    print("summary of two functions are", a + b)
+
+def func_subtraction():
+    a = float(input("Enter a first number:"))
+    b = float(input("Enter a second number:"))
+    print("subtraction of two numbers are", a - b)
+
+def func_multiplying():
+    a = float(input("Enter a first number:"))
+    b = float(input("Enter a second number:"))
+    print("multiplying of two numbers are", a * b)
+
+def func_division():
+    a = float(input("Enter a first number:"))
+    b = float(input("Enter a second number:"))
+    print("division of two numbers are", a / b)
