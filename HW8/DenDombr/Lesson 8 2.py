f92dfb37dbf73285ca7b6d5e4f536fe45f0096cf
DenysDombrovskyi <110053704+DenysDombrovskyi@users.noreply.github.com>
2022-08-23 18:25:57 +0300

    Update Lesson 8 2.py

--- HW8/DenDombr/Lesson 8 2.py
+++ HW8/DenDombr/Lesson 8 2.py
@@ -1,14 +1,7 @@
 import re
-pattern_password = re.compile(r'^(?=.*[0-9].*)(?=.*[a-z].*)(?=.*[A-Z].*)[0-9a-zA-Z]{8,}$')
-
-print(bool(pattern_password.match('absghk4D')))  # True
-print(bool(pattern_password.match('abc123FF')))  # True
-print(bool(pattern_password.match('123ABCac')))  # True
-print(bool(pattern_password.match('abcFF123')))  # True
-print()
-print(bool(pattern_password.match('absghk4D $%#$')))  # False
-print(bool(pattern_password.match('')))               # False
-print(bool(pattern_password.match('bsghk4D')))        # False
-print(bool(pattern_password.match('abc_aaFF')))       # False
-print(bool(pattern_password.match('abcabcac')))       # False
-print(bool(pattern_password.match('ABCDF!@##')))      # False
+password=input('Enter your password: ')
+pattern='^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[$#@]).{6,16}$'
+if re.fullmatch(pattern, password):
+  print('Congrats! There is a match.')
+else:
+  print('Wrong password')
