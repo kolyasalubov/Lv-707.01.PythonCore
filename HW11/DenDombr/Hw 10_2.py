4997a3696045c0876edf0091b0b0b0f8c548ed51
DenysDombrovskyi <110053704+DenysDombrovskyi@users.noreply.github.com>
2022-08-29 12:38:25 +0300

    Add files via upload

---
+++ HW11/DenDombr/Hw 10_2.py
@@ -0,0 +1,29 @@
+class Human:
+    '''
+    Create a class Human, everyone has a name, create a
+    method in the class that displays a welcome message to a
+    each person. Create a class method in the class that
+    returns information that it is a species of "Homosapiens".
+    And also in the class create a static method that returns
+    an arbitrary message
+    '''
+    def __init__(self,name) -> None:
+        self.name = name
+    
+    def greeting(self):
+        return f"Hello dear {self.name} you are in instance method"
+
+    @classmethod
+    def class_of_method(cls):
+        return "It's kind of Homosapiens",cls
+        
+
+    @staticmethod
+    def static_of_method():
+        return 'Good news for starting some new!'
+
+human1 = Human('Andrii')
+print(human1.greeting())
+print(human1.class_of_method())
+print(Human.class_of_method())  
+print(human1.static_of_method())
