8fcfe0e9f87021c67e4fc1644a04c3441f993b8a
Vasyl Shulevka <shulvasya@gmail.com>
2022-08-31 15:06:38 +0300

    added 2 lines

--- HW10/shvasya/class.py
+++ HW10/shvasya/class.py
@@ -3,7 +3,7 @@ class Human:
         self.name = name
 
     def hello(self):
-        return "Hello user {self.name}"   
+        return f"Hello user {self.name}"   
 
     @classmethod
     def methodClass(cls):
@@ -12,3 +12,10 @@ class Human:
     @staticmethod
     def any_message():
         return "I hope I can become a coder"    
+
+s = Human("Vasya")
+s.hello
+s.any_message
+print(s.hello())
+print(s.any_message())
+print(Human.methodClass())
