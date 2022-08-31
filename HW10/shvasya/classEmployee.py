e2a3105aac7473625c8def796ea3a25c0bacbc1f
Vasyl Shulevka <shulvasya@gmail.com>
2022-08-31 20:53:25 +0300

    added employee class

---
+++ HW10/shvasya/classEmployee.py
@@ -0,0 +1,30 @@
+class Employee():
+    """
+    This class is about the employee's information
+    """
+
+    counter=0
+    def __init__(self, name, salary):
+        Employee.counter +=1
+        self.__name = name
+        self.__salary = salary
+    def __del__(self):
+        Employee.counter -=1    
+
+    def total_employee(self):
+        return Employee.counter
+
+    def name_of_employee(self):
+        print(f"Name of employee is {self.__name} and his or her salary is {self.__salary}")    
+
+
+
+m1 = Employee('Vasya', 200)        
+m2 = Employee('Katya', 300)
+print(Employee.counter)
+m1.name_of_employee()
+print(Employee.__base__)
+print(Employee.__dict__)
+print(Employee.__name__)
+print(Employee.__module__)
+print(Employee.__doc__)
