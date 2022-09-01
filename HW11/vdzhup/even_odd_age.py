b5ac47f3903b8e657a65d74123fc1762c37d69c4
Volodymyr Dzhupyna <vdzhupyna@gmail.com>
2022-09-01 16:19:04 +0300

    added changes in raise and except blocks

--- HW11/vdzhup/even_odd_age.py
+++ HW11/vdzhup/even_odd_age.py
@@ -2,9 +2,9 @@ def check_age():
     try:
         input_age = int(input("Enter your age: "))
         if input_age < 0:
-            raise ValueError("You entered incorrect data.")
-    except ValueError:
-        return "You entered incorrect data."
+            raise ValueError("Age must be positive!")
+    except ValueError as e:
+        return f"You entered incorrect data: {e}"
     else:
         return f"Age {input_age} is even" if input_age % 2 == 0 else f"Age {input_age} is odd"
 
