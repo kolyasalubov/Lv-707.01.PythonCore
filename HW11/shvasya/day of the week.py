3fd909ff8563ab1cdb75246d6c17b818ec011244
Vasyl Shulevka <shulvasya@gmail.com>
2022-09-05 12:37:27 +0300

    added 2 lines

--- HW11/shvasya/day of the week.py
+++ HW11/shvasya/day of the week.py
@@ -1,13 +1,16 @@
+class CustomError(Exception):
+    def __init__(self,data):
+        self.data = data
 number = int(input("enter your number:"))
 try:
   if number <= 0:
     raise ValueError("your number can't be negative")
   if number > 7:
-    raise Exception("days in the week can't be bigger than 8")  
+    raise CustomError("days in the week can't be bigger than 8")  
 except ValueError as e:
   print("Your Error is", e)    
-except Exception as e:
-  print("Your error is that", e)  
+except CustomError as e:
+  print("Your error is that", e.data)  
 else:
   corresp_days = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 
                5: 'Saturday', 6: 'Friday', 7: 'Saturday'}
