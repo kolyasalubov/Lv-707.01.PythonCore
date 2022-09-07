fae3118ec3541264455d22f59c8e3f33ede63d7d
Basil <karbanvasyl@gmail.com>
2022-09-07 14:38:20 +0300

    Project enhancement

--- Project/vaskar/frequency.py
+++ Project/vaskar/frequency.py
@@ -1,20 +1,34 @@
-from math import sin, cos, sqrt, atan2, radians
 import geopy
 from geopy import distance
 geolocator = geopy.Nominatim(user_agent="tutorial")
-loads = ['Memphis TN', 'Jacksonville FL', 'ALLENTOWN PA', 'DALLAS TX', 'BURLINGTON NJ', 'WAYNE NJ', \
-'SOMERSET NJ', 'TOPEKA KS', 'KEARNS UT', 'HORN LAKE MS', 'DAYTON NJ', 'JONESBORO AR', 'FAYETTEVILLE TN', \
-     'LACOMBE LA', 'PERRY FL', 'OCALA FL', 'JOLIET IL', 'PULASKI TN', 'KATHLEEN GA' ]
-longitudes = [geolocator.geocode(a).longitude for a in loads]
-latitudes = [geolocator.geocode(a).latitude for a in loads]
-frequency = {}
-for l, a, b in zip(loads, latitudes, longitudes):
-    for g,f in zip(latitudes, longitudes):
-        if distance.distance((a,b),(g,f)).miles <50:
-            if l in frequency:
-                frequency[l] +=1
+# pu = ['Memphis TN', 'Jacksonville FL', 'ALLENTOWN PA', 'DALLAS TX', 'BURLINGTON NJ', 'WAYNE NJ', \
+# 'SOMERSET NJ', 'TOPEKA KS', 'KEARNS UT', 'HORN LAKE MS', 'DAYTON NJ', 'JONESBORO AR', 'FAYETTEVILLE TN', \
+#      'LACOMBE LA', 'PERRY FL', 'OCALA FL', 'JOLIET IL', 'PULASKI TN', 'KATHLEEN GA' ]
+# deliv = ['Fresno CA', 'Cincinnati OH', 'Laredo TX', 'Atlanta GA', 'Mira Loma CA', 'Houston TX', \
+#     'Chattanooga TN', 'Chicago IL',  'York PA', 'Fairport NY', 'Fort Worth TX', 'Rosenberg TX', 'Princeton KY', \
+#          'Orlando FL', 'Phoenix AZ', 'Charlotte NC',  'Binghamton NY', 'Portland OR' 'Modesto CA', 'West Columbia SC' ]
+pu = ['Memphis TN', 'Jacksonville FL', 'ALLENTOWN PA', 'ALLENTOWN PA', 'Hellertown PA']
+deliv = ['Fresno CA', 'Cincinnati OH', 'Laredo TX', 'Laredo TX', 'Rio Bravo TX' ]
+lanes = [str(a + " " +b) for a,b in zip(pu, deliv)]
+print(lanes)
+pu_longitudes = [geolocator.geocode(a).longitude for a in pu]
+pu_latitudes = [geolocator.geocode(a).latitude for a in pu]
+del_longitudes = [geolocator.geocode(a).longitude for a in deliv]
+del_latitudes = [geolocator.geocode(a).latitude for a in deliv]
+lane_frequency = {}
+accounted_lanes = []
+for l, a, b, c, d in zip(lanes, pu_latitudes, pu_longitudes, del_latitudes, del_longitudes):
+    if l in accounted_lanes:
+        continue
+    else:
+        for s, g,f,r,p in zip(lanes, pu_latitudes, pu_longitudes, del_latitudes, del_longitudes):
+            print(distance.distance((a,b),(g,f)).miles, distance.distance((c,d),(r,p)).miles, l, s)
+            if distance.distance((a,b),(g,f)).miles <50 and distance.distance((c,d),(r,p)).miles < 50:
+                if l in lane_frequency:
+                    lane_frequency[l] +=1
+                else:
+                    lane_frequency[l] = 1
             else:
-                frequency[l] = 1
-        else:
-            continue
-print(frequency)
+                continue
+    accounted_lanes.append(l)
+print(lane_frequency)
