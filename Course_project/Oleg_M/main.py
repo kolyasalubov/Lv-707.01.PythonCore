f9b32f6cd50e3a5761829c30c8a68396a39f4f12
“Oleg <ttest261@gmail.com>
2022-09-07 14:58:05 +0300

    add comment to course_project

--- Course_project/Oleg_M/main.py
+++ Course_project/Oleg_M/main.py
@@ -7,11 +7,12 @@ import json
 
 amount_numbers_min = 1_000
 amount_numbers_max = 10_000
-amount_step = 500
-max_vol = 100_000
-__LIST_OF_SORT = {s.MergeSort, s.QuickSort}
-#__LIST_OF_SORT = {s.SelectionSort, s.InsertionSort, s.MergeSort, s.QuickSort}
-#__LIST_OF_SORT = {s.SelectionSort, s.InsertionSort, s.MergeSort, s.QuickSort, s.RadixSort, s.BubbleSort, s.BubbleSortWithBrake}
+amount_step = 300
+max_vol = 5_000
+# __LIST_OF_SORT = {s.MergeSort, s.QuickSort}
+__LIST_OF_SORT = {s.SelectionSort, s.InsertionSort, s.MergeSort, s.QuickSort}
+#__LIST_OF_SORT = {s.SelectionSort, s.InsertionSort, s.MergeSort, s.QuickSort, s.RadixSort, s.BubbleSort,
+#                  s.BubbleSortWithBrake}
 
 __DICT_OF_RESULT = {}
 
@@ -21,16 +22,17 @@ for iteration in range(amount_numbers_min, amount_numbers_max + 1, amount_step):
     _data = [r.randint(1, max_vol) for _ in range(iteration)]
 
     for type_of_alg in __LIST_OF_SORT:
+        # create object of sort
         alg = type_of_alg(_data.copy())
 
+        # sorting and calculate time
         # print(alg.data)
         time_begin = t.time()
-
         alg.start_sorting(alg.data)
-
         time_sort = t.time() - time_begin
         # print(alg.data)
 
+        #init result  dictionary
         if not __DICT_OF_RESULT.get(type_of_alg.__name__, False):
             __DICT_OF_RESULT[type_of_alg.__name__] = [(iteration, time_sort)]
         else:
@@ -40,16 +42,16 @@ for iteration in range(amount_numbers_min, amount_numbers_max + 1, amount_step):
 
 p.pprint(__DICT_OF_RESULT)
 
-#Save in JSON file
+# Save in JSON file
 with open("create_data_file.json", "w") as write_file:
     json.dump(__DICT_OF_RESULT, write_file)
 
-#Load from JSON file
+# Load from JSON file
 with open("create_data_file.json", "r") as read_file:
     __DICT_OF_RESULT = json.load(read_file)
 
-#Show plot
-plt.xkcd()
+# Show plot
+
 for type_of_alg in __LIST_OF_SORT:
     ages_y = [type_alg[1] for type_alg in __DICT_OF_RESULT.get(type_of_alg.__name__)]
     ages_x = [type_alg[0] for type_alg in __DICT_OF_RESULT.get(type_of_alg.__name__)]
