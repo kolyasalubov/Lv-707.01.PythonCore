f29af95831cf4a4dba52dfb471add3e80cb3b6d5
Volodymyr Dzhupyna <vdzhupyna@gmail.com>
2022-09-08 16:04:37 +0300

    change variable UNITS_OF_MEASURE

--- bot_project/calculate.py
+++ bot_project/calculate.py
@@ -1,7 +1,7 @@
 def get_tomato_marinade(user_value):
     ingredients_tomato = {"вода": 1.5, "цукор": 6, "оцет": 85, "сіль": 1.5}
     new_ingredients_tomato = []
-    units_of_measure =  [" л.", " ст.л.", " мл.", " ст.л."]
+    UNITS_OF_MEASURE =  [" л.", " ст.л.", " мл.", " ст.л."]
 
     for key, value in ingredients_tomato.items():
         ingredients_tomato[key] = round((value * user_value) / 1.5, 1)
@@ -10,16 +10,15 @@ def get_tomato_marinade(user_value):
         new_ingredients_tomato.append(f"{key} - {value}")
     
     for item in range(len(new_ingredients_tomato)):
-        new_ingredients_tomato[item] += units_of_measure[item]
+        new_ingredients_tomato[item] += UNITS_OF_MEASURE[item]
         
     result = "; ".join(new_ingredients_tomato)
-    
     return result
 
 def get_cucumber_marinade(user_value):
     ingredients_cucmber = {"вода": 1, "цукор": 100, "оцет": 120, "сіль": 1}
     new_ingredients_cucumber = []
-    units_of_measure =  [" л.", " г.", " мл.", " ст.л."]
+    UNITS_OF_MEASURE =  [" л.", " г.", " мл.", " ст.л."]
 
     for key, value in ingredients_cucmber.items():
         ingredients_cucmber[key] = round(value * user_value, 2)
@@ -28,8 +27,7 @@ def get_cucumber_marinade(user_value):
         new_ingredients_cucumber.append(f"{key} - {value}")
     
     for item in range(len(new_ingredients_cucumber)):
-        new_ingredients_cucumber[item] += units_of_measure[item]
+        new_ingredients_cucumber[item] += UNITS_OF_MEASURE[item]
         
     result = "; ".join(new_ingredients_cucumber)
-
     return result
